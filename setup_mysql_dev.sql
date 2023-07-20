1c6d26a3b3a7f7fb45d6ec585f53a19047b1a035
Yordi SE <yordanoslemmaworkofkind1@gmail.com>
2023-07-20 12:41:57 -0700

    mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,9 @@
+-- create database and prepares a MySQL server for the project
+-- create database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- create user hbnb_dev
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant all privileges to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- grant select privileges on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
