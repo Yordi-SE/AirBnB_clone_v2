db518aed35d3225bdf46b19a3c62006e495972ea
Yordi SE <yordanoslemmaworkofkind1@gmail.com>
2023-07-31 01:51:49 -0700

    web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Flask web application framework
+"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def Hello():
+    """This is the view
+    function
+    """
     return 'Hello HBNB!'
 
 
